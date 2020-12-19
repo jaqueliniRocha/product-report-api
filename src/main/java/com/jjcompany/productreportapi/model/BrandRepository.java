b75eeacdd5ca712624398ab7ebd425a8006327f6
jaqueliniRocha <jaquelini.rocha@hotmail.com>
2020-12-19 17:53:02 -0300

    delete brandrepository

--- src/main/java/com/jjcompany/productreportapi/model/BrandRepository.java
+++
@@ -1,7 +0,0 @@
-package com.jjcompany.productreportapi.model;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BrandRepository extends JpaRepository<Brand, Long>{
-
-}
